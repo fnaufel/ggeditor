6355bc3042b72d4eb28187296dc968e5cd265d0a
Fernando Naufel <fnaufel@gmail.com>
2021-10-14 18:31:03 -0300

    Start implementing edit_text handler

--- ggeditor/public_html/js/default.js
+++ ggeditor/public_html/js/default.js
@@ -1,3 +1,4 @@
+// deno-lint-ignore-file no-var no-unused-vars
 /* 
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -5,9 +6,41 @@
  */
 
 $(document).ready(function(){
-	//code here...
+    // Editor
 	var code = $(".codemirror-textarea")[0];
 	var editor = CodeMirror.fromTextArea(code, {
 		lineNumbers : true
 	});
+
+    // Add event handler for edit-text button
+    $("#edit_text").click(handle_edit_text);
+
 });
+
+// Edit existing text object
+function handle_edit_text(e) {
+
+    // Clear selection list (except for first option)
+    $('#edit_text option')
+        .filter(function(index){ return index > 0; })
+        .remove();
+
+    // Get list of text objects from app
+    var app = applet.getAppletObject();
+    var objs = app.getAllObjectNames('text');
+
+    et = $('#edit_text');
+
+    // Populate selection list with text objects from the app
+    $.each(objs, function(i, obj) {
+        var op = $('<option/>')
+            .attr('value', obj)
+            .text(obj)
+            .prop('selected', i == 1);
+
+        et.append(op);
+    });
+    
+}
+
+
